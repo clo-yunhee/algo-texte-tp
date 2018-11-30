ede17a797e5fc7efcdeed32c770e462530df9df0
Chloé Ichinose <chloedv99@gmail.com>
2018-11-30 10:21:40 +0100

    Some script changes

--- TP3/package.sh
+++ TP3/package.sh
@@ -5,4 +5,4 @@ ARK=ProjetAlgoTxt.tgz
 tar cjvf $ARK --exclude-tag=.nodir \
     bin src inc Makefile .gitignore \
     generator/bin generator/src generator/inc generator/Makefile generator/.gitignore \
-    mots.txt texte.txt script.sh package.sh
+    mots.txt texte.txt script.sh package.sh toggle-test.sh
--- TP3/script.sh
+++ TP3/script.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 make TRIE=hachage
 make TRIE=matrice
+make generator
 bin/genere-texte 50000 3 > texte3.txt
 bin/genere-mots 10 5 25 3 > mots3.txt
 bin/ac-matrice mots3.txt texte3.txt > res-ac-matrice
