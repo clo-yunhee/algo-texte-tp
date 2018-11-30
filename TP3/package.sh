78e4181e170a5619905d6adcd7037cdbefa73cde
Chloé Ichinose <chloedv99@gmail.com>
2018-11-30 10:19:54 +0100

    Added files to create tgz package

---
+++ TP3/package.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ARK=ProjetAlgoTxt.tgz
+
+tar cjvf $ARK --exclude-tag=.nodir \
+    bin src inc Makefile .gitignore \
+    generator/bin generator/src generator/inc generator/Makefile generator/.gitignore \
+    mots.txt texte.txt script.sh package.sh
